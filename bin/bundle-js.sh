0fc9659df2c7abb22f4060678629e671e3d8832b
Brandon Parmenter <leftiness@gmail.com>
2015-10-04 13:34:24 -0400

    Fixed bundle-js script
    Suddenly the full path was needed...

--- bin/bundle-js.sh
+++ bin/bundle-js.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
+BOWER='./bower_components/'
 DEPS=(
-	'angular'
-	'angular-animate'
-	'angular-aria'
-	'angular-cookies'
-	'angular-material'
-	'angular-resource'
-	'angular-sanitize'
-	'angular-ui-router'
-	'lodash'
-	'restangular'
+	'angular/angular.js'
+	'angular-animate/angular-animate.js'
+	'angular-aria/angular-aria.js'
+	'angular-cookies/angular-cookies.js'
+	'angular-material/angular-material.js'
+	'angular-resource/angular-resource.js'
+	'angular-sanitize/angular-sanitize.js'
+	'angular-ui-router/release/angular-ui-router.js'
+	'lodash/lodash.js'
+	'restangular/dist/restangular.js'
 )
 CONCAT=''
 
 for each in "${DEPS[@]}"
 do
-	CONCAT="$CONCAT -x $each"
+	CONCAT="$CONCAT -x $BOWER$each"
 done
 
 CMD="browserify ./app/app.js -o ./dist/bundle.js $CONCAT"
