8e40cdca5148612fc0c2df1c0133ff2d3c367542
Brandon Parmenter <leftiness@gmail.com>
2015-10-10 02:26:19 -0400

    Configure server for OpenShift

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var app = express();
 var options = {
 	root: __dirname + '/dist/',
 }
+var port = process.env.OPENSHIFT_NODEJS_PORT || 5000;
+var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.use('/assets', express.static(__dirname + '/assets'));
 app.use(express.static(__dirname + '/dist'));
@@ -13,5 +15,6 @@ app.all('/*', function (req, res) {
 	res.sendFile('index.html', options);
 });
 
-app.listen('5000');
-console.log('All systems are go!');
+app.listen(port, ip, function () {
+	console.log('All systems are go!');
+});
