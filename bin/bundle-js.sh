b7bd72009b2592b9b09f9cc87a345791fdff22fe
Brandon Parmenter <leftiness@gmail.com>
2015-10-10 03:10:24 -0400

    More Bash for heroku
    Why did I sync before double checking...?

--- bin/bundle-js.sh
+++ bin/bundle-js.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 BOWER='./bower_components/'
-DEPS=(
-	'angular/angular.js'
-	'angular-animate/angular-animate.js'
-	'angular-aria/angular-aria.js'
-	'angular-cookies/angular-cookies.js'
-	'angular-material/angular-material.js'
-	'angular-resource/angular-resource.js'
-	'angular-sanitize/angular-sanitize.js'
-	'angular-ui-router/release/angular-ui-router.js'
-	'lodash/lodash.js'
-	'restangular/dist/restangular.js'
-)
+DEPS[0]='angular/angular.js'
+DEPS[1]='angular-animate/angular-animate.js'
+DEPS[2]='angular-aria/angular-aria.js'
+DEPS[3]='angular-cookies/angular-cookies.js'
+DEPS[4]='angular-material/angular-material.js'
+DEPS[5]='angular-resource/angular-resource.js'
+DEPS[6]='angular-sanitize/angular-sanitize.js'
+DEPS[7]='angular-ui-router/release/angular-ui-router.js'
+DEPS[8]='lodash/lodash.js'
+DEPS[9]='restangular/dist/restangular.js'
 CONCAT=''
 
 for each in "${DEPS[@]}"
